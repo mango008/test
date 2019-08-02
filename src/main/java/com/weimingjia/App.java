5be991fb19df32c61f463e7b5c2809ddd82d487e
Mango <995779872@qq.com>
2019-08-02 12:28:20 +0800

    diyici

---
+++ src/main/java/com/weimingjia/App.java
@@ -0,0 +1,14 @@
+package com.weimingjia;
+
+import java.util.LinkedList;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        LinkedList<Object> fatherList = new LinkedList<>();
+
+    }
+}
